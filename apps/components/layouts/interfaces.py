03f26d3a248ef6936df4c86d871bdd3240b06caa
WeberTPI <weber.huang@tpisoftware.com>
2025-01-10 17:25:30 +0800

    remove old modu;es

--- apps/components/layouts/interfaces.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Layout(ABC):
-    
-    @abstractmethod
-    def __call__(self, *args, **kwargs):
-        raise NotImplementedError
--- apps/utils/data/pipelines/txt_to_html.py
+++
@@ -1,9 +0,0 @@
-from ..base_pipe import Pipeline
-
-
-def get_txt_pipeline() -> Pipeline:
-    from ..loader.txt_loader import TxtLoader
-
-    loader = TxtLoader()
-    pipeline = Pipeline(load=loader)
-    return pipeline
