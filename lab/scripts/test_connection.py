72ad8ab39381d89b248ab89d6efbc60f576a5fa9
Weber <weber.huang@tpisoftware.com>
2025-01-11 10:13:46 +0800

    udpate the settings

---
+++ lab/scripts/test_connection.py
@@ -0,0 +1,25 @@
+import os
+from openai import OpenAI
+
+
+
+client = OpenAI(
+    base_url="http://vllm-server:9999/v1",
+    api_key="12345",
+)
+
+stream = client.chat.completions.create(
+    model="Breeze-7B",
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant."
+        },
+        {
+            "role": "user",
+            "content": "Hello!"
+        }
+    ],
+)
+
+print(stream)
